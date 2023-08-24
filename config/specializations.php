ca096cdf6ae5734cba748f0e396d559356cfcb5e
Pierluca-Strianese <pierluca.strianese@gmail.com>
2023-08-25 01:37:42 +0200

    fix specializations.php

---
+++ config/specializations.php
@@ -0,0 +1,72 @@
+<?php
+return[
+    [
+        "name" => "Cardiologia",
+    ],
+    [
+        "name" => "Ortopedia e Medicina dello sport",
+    ],
+    [
+        "name" => "Pneumologia",
+    ],
+    [
+        "name" => "Pediatria",
+    ],
+    [
+        "name" => "Oncologia",
+    ],
+    [
+        "name" => "Ematologia",
+    ],
+    [
+        "name" => "Psichiatria",
+    ],
+    [
+        "name" => "Dermatologia",
+    ],
+    [
+        "name" => "Neurologia",
+    ],
+    [
+        "name" => "Ginecologia",
+    ],
+    [
+        "name" => "Medicina legale, del lavoro, igiene",
+    ],
+    [
+        "name" => "Malattie infettive",
+    ],
+    [
+        "name" => "Endocrinologia",
+    ],
+    [
+        "name" => "Chirurgia generale",
+    ],
+    [
+        "name" => "Medicina di base",
+    ],
+    [
+        "name" => "Diagnostica",
+    ],
+    [
+        "name" => "Infettologia",
+    ],
+    [
+        "name" => "Nefrologia",
+    ],
+    [
+        "name" => "Oncologia",
+    ],
+    [
+        "name" => "Immunologia",
+    ],
+    [
+        "name" => "Terapia Intensiva",
+    ],
+    [
+        "name" => "Chirurgia Plastica",
+    ],
+    [
+        "name" => "Medicina Interna",
+    ],
+];
