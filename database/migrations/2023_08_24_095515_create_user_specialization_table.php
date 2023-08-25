2a1e6ae63cb6e395ecb8b71be03aae4b3986a7eb
Paolo96700 <paolo.falcoapp@gmail.com>
2023-08-25 11:15:42 +0200

    Revert "add router link"
    This reverts commit 09f7faa4a93cb9094a8a82161d537164ad114122.

--- database/migrations/2023_08_24_095515_create_user_specialization_table.php
+++ database/migrations/2023_08_24_095515_create_user_specialization_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::create('user_specialization', function (Blueprint $table) {
+            $table->id();
+
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('specialization_id');
 
