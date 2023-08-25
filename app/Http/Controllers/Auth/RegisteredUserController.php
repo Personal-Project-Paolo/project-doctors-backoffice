e0441b6a3c42dcb88186749416de86c8e6c52a3b
Paolo96700 <paolo.falcoapp@gmail.com>
2023-08-25 16:55:22 +0200

    prova register

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,12 +32,15 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
+            'lastname' => 'required'|'string'|'max:255',
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+
         ]);
 
         $user = User::create([
             'name' => $request->name,
+            'name' => $request->lastname,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
