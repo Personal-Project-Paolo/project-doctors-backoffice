b8b02cb3a3fed3d9b60937ae6d1e454c07dcc057
Alessandra-Casu <alessandracasu919@gmail.com>
2023-09-08 12:51:52 +0200

    add new doctors and new users

--- config/users.php
+++ config/users.php
@@ -8,7 +8,7 @@ return [
         "lastname" => "Dorian",
         "address" => "Via numero 1 - Città 1",
 
-        "specializations" => [23],
+        "specializations" => [22],
     ],
     [
         "email" => "gregoryhouseisthebest@mail.com",
@@ -17,7 +17,7 @@ return [
         "lastname" => "House",
         "address" => "Via numero 2 - Città 2",
         
-        "specializations" => [16, 17, 18],
+        "specializations" => [15, 16, 17],
     ],
     [
         "email" => "J.Wilson@mail.com",
@@ -26,7 +26,7 @@ return [
         "lastname" => "Wilson",
         "address" => "Via numero 3 - Città 3",
         
-        "specializations" => [19],
+        "specializations" => [18],
     ],
     [
         "email" => "eric.foreman@mail.com",
@@ -35,7 +35,7 @@ return [
         "lastname" => "Foreman",
         "address" => "Via numero 4 - Città 4",
         
-        "specializations" => [16, 20],
+        "specializations" => [15, 19],
     ],
     [
         "email" => "martha.masters@mail.com",
@@ -44,7 +44,7 @@ return [
         "lastname" => "Masters",
         "address" => "Via numero 5 - Città 5",
         
-        "specializations" => [16, 14],
+        "specializations" => [15, 13],
     ],
     [
         "email" => "robert.chase.@mail.com",
@@ -53,7 +53,7 @@ return [
         "lastname" => "Chase",
         "address" => "Via numero 6 - Città 6",
        
-        "specializations" => [14, 16, 21],
+        "specializations" => [13, 15, 20],
     ],
     [
         "email" => "allison.cameron@mail.com",
@@ -62,7 +62,7 @@ return [
         "lastname" => "Cameron",
         "address" => "Via numero 7 - Città 7",
         
-        "specializations" => [16, 20],
+        "specializations" => [15, 19],
     ],
     [
         "email" => "remy.hadley13@mail.com",
@@ -71,7 +71,7 @@ return [
         "lastname" => "",
         "address" => "Via numero 8 - Città 8",
         
-        "specializations" => [16, 23],
+        "specializations" => [15, 22],
     ],
     [
         "email" => "chris.taub @mail.com",
@@ -80,7 +80,7 @@ return [
         "lastname" => "Taub",
         "address" => "Via numero 9 - Città 9",
         
-        "specializations" => [16, 22],
+        "specializations" => [15, 21],
     ],
     [
         "email" => "l.kutner@mail.com",
@@ -98,7 +98,7 @@ return [
         "lastname" => "Reid",
         "address" => "Via numero 11 - Città 11",
         
-        "specializations" => [4, 15],
+        "specializations" => [4, 14],
     ],
     [
         "email" => "christopher.turk@mail.com",
@@ -107,7 +107,7 @@ return [
         "lastname" => "Turk",
         "address" => "Via numero 12 - Città 12",
         
-        "specializations" => [14, 1],
+        "specializations" => [13, 1],
     ],
     [
         "email" => "perry.cox@mail.com",
@@ -116,7 +116,7 @@ return [
         "lastname" => "Cox",
         "address" => "Via numero 13 - Città 13",
         
-        "specializations" => [15, 7],
+        "specializations" => [14, 6],
     ],
     [
         "email" => "Bob.Kelso.@mail.com",
@@ -125,15 +125,246 @@ return [
         "lastname" => "Kelso",
         "address" => "Via numero 14 - Città 14",
         
-        "specializations" => [10, 17],
+        "specializations" => [9, 16],
     ],
     [
         "email" => "iltodd@mail.com",
-        "password" => "15222222",
+        "password" => "00000001",
         "name" => "Todd",
         "lastname" => "Quinlan",
         "address" => "Via numero 15 - Città 15",
         
-        "specializations" => [10, 13, 17],
+        "specializations" => [9, 12, 16],
     ],
+    [
+        "email" => "dottalzheimer@mail.com",
+        "password" => "00000002",
+        "name" => "Helmut",
+        "lastname" => "Alzheimer",
+        "address" => "Via numero 16 - Città 16",
+        
+        "specializations" => [13],
+    ],
+    [
+        "email" => "dougross@mail.com",
+        "password" => "00000003",
+        "name" => "Doug",
+        "lastname" => "Ross",
+        "address" => "Via numero 17 - Città 17",
+        
+        "specializations" => [22],
+    ],
+    [
+        "email" => "kingshulz@mail.com",
+        "password" => "00000004",
+        "name" => "King",
+        "lastname" => "Shulz",
+        "address" => "Via numero 18 - Città 18",
+        
+        "specializations" => [23],
+    ],
+    [
+        "email" => "albfumagalli@mail.com",
+        "password" => "00000005",
+        "name" => "Alberto",
+        "lastname" => "Fumagalli",
+        "address" => "Via numero 19 - Città Aosta",
+        
+        "specializations" => [10],
+    ],
+    [
+        "email" => "cristinayang@mail.com",
+        "password" => "00000006",
+        "name" => "Cristina",
+        "lastname" => "Yang",
+        "address" => "Via numero 20 - Città 20",
+        
+        "specializations" => [24],
+    ],
+    [
+        "email" => "izziestevens@mail.com",
+        "password" => "00000007",
+        "name" => "Izzie",
+        "lastname" => "Stevens",
+        "address" => "Via numero 21 - Città 21",
+        
+        "specializations" => [18],
+    ],
+    [
+        "email" => "shaunmurphy@mail.com",
+        "password" => "00000008",
+        "name" => "Shaun",
+        "lastname" => "Murphy",
+        "address" => "Via numero 22 - Città 22",
+        
+        "specializations" => [13],
+    ],
+    [
+        "email" => "luigizampa@mail.com",
+        "password" => "00000009",
+        "name" => "Luigi",
+        "lastname" => "Zampa",
+        "address" => "Via numero 23 - Roma",
+        
+        "specializations" => [14],
+    ],
+    [
+        "email" => "peterbenton@mail.com",
+        "password" => "00000010",
+        "name" => "Peter",
+        "lastname" => "Benton",
+        "address" => "Via numero 24 - Città 24",
+        
+        "specializations" => [13],
+    ],
+    [
+        "email" => "giorgiabasile@mail.com",
+        "password" => "00000011",
+        "name" => "Giorgia",
+        "lastname" => "Basile",
+        "address" => "Via numero 25 - Città 25",
+        
+        "specializations" => [9],
+    ],
+    [
+        "email" => "marksloan@mail.com",
+        "password" => "00000012",
+        "name" => "Mark",
+        "lastname" => "Sloan",
+        "address" => "Via numero 26 - Città 26",
+        
+        "specializations" => [21],
+    ],
+    [
+        "email" => "stephenstrange@mail.com",
+        "password" => "00000012",
+        "name" => "Stephen",
+        "lastname" => "Strange",
+        "address" => "Via numero 27 - Città 27",
+        
+        "specializations" => [25],
+    ],
+    [
+        "email" => "hunteradams@mail.com",
+        "password" => "00000013",
+        "name" => "Hunter Patch",
+        "lastname" => "Adams",
+        "address" => "Via numero 28 - Città 28",
+        
+        "specializations" => [4],
+    ],
+    [
+        "email" => "dottmale@mail.com",
+        "password" => "00000014",
+        "name" => "Douglas",
+        "lastname" => "Power",
+        "address" => "Via numero 29 - Città 29",
+        
+        "specializations" => [4],
+    ],
+    [
+        "email" => "johndolittle@mail.com",
+        "password" => "00000015",
+        "name" => "John",
+        "lastname" => "Dolittle",
+        "address" => "Via numero 30 - Città 30",
+        
+        "specializations" => [13],
+    ],
+    [
+        "email" => "ottoguntheroctavius@mail.com",
+        "password" => "00000016",
+        "name" => "Octavius Gunther",
+        "lastname" => "Otto",
+        "address" => "Via numero 31 - Città 31",
+        
+        "specializations" => [8],
+    ],
+    [
+        "email" => "emmettlathropbrown@mail.com",
+        "password" => "00000017",
+        "name" => "Emmett",
+        "lastname" => "Lathrop Brown",
+        "address" => "Via numero 32 - Città 32",
+        
+        "specializations" => [15],
+    ],
+    [
+        "email" => "guidozanin@mail.com",
+        "password" => "00000018",
+        "name" => "Guido",
+        "lastname" => "Zanin",
+        "address" => "Via numero 33 - Città 33",
+        
+        "specializations" => [14],
+    ],
+    [
+        "email" => "jackshephard@mail.com",
+        "password" => "00000019",
+        "name" => "Jack",
+        "lastname" => "Shephard",
+        "address" => "Via numero 34 - Città 34",
+        
+        "specializations" => [25],
+    ],
+    [
+        "email" => "meredithgreyd@mail.com",
+        "password" => "00000020",
+        "name" => "Meredith",
+        "lastname" => "Grey",
+        "address" => "Via numero 35 - Città 35",
+        
+        "specializations" => [13, 20],
+    ],
+    [
+        "email" => "jalexkarev@mail.com",
+        "password" => "00000021",
+        "name" => "Alex",
+        "lastname" => "Karev",
+        "address" => "Via numero 36 - Città 36",
+        
+        "specializations" => [4],
+    ],
+    [
+        "email" => "mirandabailey@mail.com",
+        "password" => "00000022",
+        "name" => "Miranda",
+        "lastname" => "Bailey",
+        "address" => "Via numero 37 - Città 37",
+        
+        "specializations" => [22, 20],
+    ],
+    [
+        "email" => "callietorres@mail.com",
+        "password" => "00000023",
+        "name" => "Callie",
+        "lastname" => "Torres",
+        "address" => "Via numero 38 - Città 38",
+        
+        "specializations" => [2],
+    ],
+    [
+        "email" => "aprilkepner@mail.com",
+        "password" => "00000024",
+        "name" => "April",
+        "lastname" => "Kepner",
+        "address" => "Via numero 39 - Città 39",
+        
+        "specializations" => [3, 4],
+    ],
+    [
+        "email" => "jowilson@mail.com",
+        "password" => "00000024",
+        "name" => "Jo",
+        "lastname" => "Wilson",
+        "address" => "Via numero 40 - Città 40",
+        
+        "specializations" => [11, 12, 13],
+    ],
+
+    
+
+   
+    
+  
 ];
