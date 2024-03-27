3076035bda537a0391d6e752e9721a56e50b8bd2
Paolo Falco <paolo.falcoapp@gmail.com>
2024-03-27 16:21:24 +0100

    add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" href="{{ Vite::asset('storage/app/public/img/doctor-logo.png')}}">
+    <title>B-doctors</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -37,9 +37,7 @@
 
         <!-- Page Content -->
         <main>
-
             @yield('contents')
-
         </main>
     </div>
 </body>
